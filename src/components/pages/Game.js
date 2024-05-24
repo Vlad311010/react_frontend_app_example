b429f8a7855a24e6066c0aa7baa25712ca465837
Vlad <vladyabl15@gmail.com>
2024-05-24 11:13:36 +0200

    game page implementation

---
+++ src/components/pages/Game.js
@@ -0,0 +1,7 @@
+export default function Game() {
+    return (
+        <div style={{paddingTop:12+'%'}} className="h3 mb-3 fw-normalw-25 m-auto d-sm-flex justify-content-sm-center">
+            <span className=" h3 mb-3 fw-normal text-bg-primary px-5 gap-3">You Win!</span>
+        </div>
+    )
+}
